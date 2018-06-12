66d244f8b86943e391a03bd1dc018637fb187f90
Ernesto Hegi <ernesto.hegi@gmail.com>
2018-06-12 17:24:05 +0100

    Extends list handler to allow for different structures.

--- pages/helpers/list-helper.js
+++ pages/helpers/list-helper.js
@@ -1,39 +1,43 @@
 let userList = [];
 
+const returnList = list => Object.freeze(list);
+
 const listHelper = {
   resetList: index =>{
     userList[index] = [];
-    return userList[index];
+    return returnList(userList[index]);
   },
   resetLists: () =>{
     userList = [];
   },
   createUserList: index => {
-    return listHelper.resetList(index);
+    return returnList(listHelper.resetList(index));
   },
   getUserList: index => {
-    return userList[index] || [];
+    return returnList(userList[index] || []);
   },
   addItemToList: (item, list) => {
     const newList = list.slice(0);
     newList.push(item);
-    return newList;
+    return returnList(newList);
   },
   addItemToListByPosition: (item, list, position) => {
     const newList = list.slice(0);
     if (Array.isArray(newList[position]) === false) newList[position] = [];
-    newList[position].push(item);
-    return newList;
+    userList[position].push(element);
+    return returnList(newList);
   },
-  addUniqueItemToList: (item, list) => {
-    const newList = [];
-    newList.push(item);
-    return newList;
+  addItemToListByPositionWithSubcategory: (item, list, position, subcategory) => {
+    const newList = list.slice(0);
+    if (Array.isArray(newList[position]) === false) newList[position] = [];
+    if (!newList[position][subcategory]) newList[position][subcategory] = []
+    userList[position][subcategory].push(element);
+    return returnList(newList);
   },
   saveList: (index, list) => {
     const newList = list.slice(0);
     userList[index] = newList;
-    return userList;
+    return returnList(userList);
   }
 };
 
