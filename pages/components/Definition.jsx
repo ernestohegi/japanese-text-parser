ff53246dd070cb9abbc22277ee9497ea9194e045
Ernesto Hegi <ernesto.hegi@gmail.com>
2018-07-02 11:58:51 +0100

    Highlights definition.

--- pages/components/Definition.jsx
+++ pages/components/Definition.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import textHelper from '../helpers/text-helper';
+import { ThemeContext } from '../styles/theme-context';
+
+const style = {
+  marginBottom: '5px'
+};
 
 class DefinitionElement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      highlighted: false
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      highlighted: true
+    });
+
+    this.props.handleClick();
+  }
+
   render() {
     const { index, definition } = this.props;
 
@@ -12,19 +33,33 @@ class DefinitionElement extends React.Component {
     ;
 
     return (
-      <div
-        className="definition"
-        key={index}
-        onClick={this.props.handleClick}
-      >
-        <span key={`${index}-japanese`} className="definition__japanese">
-          { `${japaneseDefinition.word || ''} 「${japaneseDefinition.reading}」` }
-        </span>
+      <ThemeContext.Consumer>
+        {theme => {
+          const highlightedStyle = Object.assign(
+            {
+              backgroundColor: theme.mainColor.rgba
+            },
+            style
+          );
 
-        <span key={`${index}-english`} className="definition__english">
-          { textHelper.getEnglish(definition) }
-        </span>
-      </div>
+          return (
+            <div
+              className="definition"
+              key={index}
+              onClick={this.handleClick}
+              style={this.state.highlighted ? highlightedStyle : style}
+            >
+              <span key={`${index}-japanese`} className="definition__japanese">
+                { `${japaneseDefinition.word || ''} 「${japaneseDefinition.reading}」` }
+              </span>
+
+              <span key={`${index}-english`} className="definition__english">
+                { textHelper.getEnglish(definition) }
+              </span>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
