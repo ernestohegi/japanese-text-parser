a3e543329b42b3fc5dc82dccf0fe83749ef96781
Ernesto Hegi <ernesto.hegi@gmail.com>
2018-07-24 12:06:25 +0100

    Creates service for Tangorin.

---
+++ server/helpers/services/tangorin.js
@@ -0,0 +1,45 @@
+const cheerio = require("cheerio");
+const api = require("../api");
+
+const API_URL = "http://tangorin.com/examples/";
+
+const SELECTOR = {
+  sentencesWrapper: "#dictEntries",
+  mainSentence: ".entry",
+  japaneseSentence: ".ex-dt",
+  englishSentence: ".ex-dd"
+};
+
+/**
+ * @param {string} html
+ * @return Array
+ */
+const getSentencesFromHtml = html => {
+  const $ = cheerio.load(html);
+  const $sentences = $(SELECTOR.sentencesWrapper).find(SELECTOR.mainSentence);
+  const sentences = [];
+
+  $sentences.map((index, sentence) => {
+    const $sentence = $(sentence);
+
+    sentences.push({
+      japanese: $sentence.find(SELECTOR.japaneseSentence).text(),
+      english: $sentence.find(SELECTOR.englishSentence).text()
+    });
+  });
+
+  return sentences;
+};
+
+module.exports = {
+  /**
+   * Retrieves sentences from Weblio.
+   * Returns a promise object with an array of sentences.
+   *
+   * @param {string} url
+   * @return Promise<Array>
+   */
+  getSentencesForItem(item) {
+    return api.callUrl(`${API_URL}${item}`).then(getSentencesFromHtml);
+  }
+};
