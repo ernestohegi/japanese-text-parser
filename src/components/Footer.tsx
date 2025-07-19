6b653c368da42be49df10377a41cbd152ae13b4a
Ernesto <ernesto.hegi@gmail.com>
2025-07-19 14:44:21 +0100

    Adds header and footer.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const CURRENT_YEAR = new Date().getFullYear();
+
+const Footer = () => {
+  return <footer>Ernesto Hegi, {CURRENT_YEAR} &copy;</footer>;
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+type NavLinkProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const NavLink = ({ href, children }: NavLinkProps) => {
+  // TODO as isActive functionality
+  return <Link href={href}>{children}</Link>;
+};
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <Link href="/" className="nav-logo">
+          よちむ Yochimu
+        </Link>
+        <div className="nav-links">
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/about">About</NavLink>
+          <NavLink href="/help">Help</NavLink>
+          <a
+            className="nav-link"
+            href="https://github.com/ernestohegi/japanese-text-parser"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
