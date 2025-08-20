7d8d769ffb149e497e372f0c94f8f3c0f6f924ff
Ernesto Hegi <ernesto.hegi@gmail.com>
2025-08-20 20:08:21 +0100

    Triggers deployment

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
+      staleTime: 6 * 60 * 1000, // 6 minutes
       refetchOnMount: "always",
     },
   },
